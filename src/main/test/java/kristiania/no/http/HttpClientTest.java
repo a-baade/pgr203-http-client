e1e6e3a34376b725ed5e20ee122900786f4495a6
a-baade <andreasbaade@hotmail.com>
2021-09-14 09:55:18 +0200

    add initial test

---
+++ src/main/test/java/kristiania/no/http/HttpClientTest.java
@@ -0,0 +1,15 @@
+package kristiania.no.http;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class HttpClientTest {
+
+    @Test
+    void dummyTest() {
+        assertEquals(42, 6*7);
+    }
+
+
+}
