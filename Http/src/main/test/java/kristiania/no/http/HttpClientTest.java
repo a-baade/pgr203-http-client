e529de217bb3743590d216cc1b0be6dfac5d0c8b
a-baade <andreasbaade@hotmail.com>
2021-09-14 11:04:09 +0200

    Run Test report with Github actions

--- Http/src/main/test/java/kristiania/no/http/HttpClientTest.java
+++ Http/src/main/test/java/kristiania/no/http/HttpClientTest.java
@@ -2,7 +2,7 @@ package kristiania.no.http;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class HttpClientTest {
 
